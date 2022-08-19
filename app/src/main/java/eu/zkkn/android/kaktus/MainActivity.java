c2e4c051a34d3498db8aef949ace98c2124daba9
Zdeněk Kořán <zkoran@gmail.com>
2022-08-19 16:17:53 +0200

    upgrade dependencies

--- app/src/main/java/eu/zkkn/android/kaktus/MainActivity.java
+++ app/src/main/java/eu/zkkn/android/kaktus/MainActivity.java
@@ -224,7 +224,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
@@ -313,7 +313,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                     @Override
-                    public void onFailure(Throwable t) {
+                    public void onFailure(@NonNull Throwable t) {
                         // Do nothing
                     }
                 },
