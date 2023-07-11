7701ae9f403070bcd239e05489d29fb806738d30
Zdeněk Kořán <zkoran@gmail.com>
2023-07-11 19:33:58 +0200

    add another RegEx to match text on web

--- backend/src/test/java/eu/zkkn/android/kaktus/backend/CheckServletTest.java
+++ backend/src/test/java/eu/zkkn/android/kaktus/backend/CheckServletTest.java
@@ -23,5 +23,7 @@ class CheckServletTest {
         assertTrue(CheckServlet.textMatchesPattern("Stačí dobít dnes 20. 6. mezi 15 a 17 hodinou 200 - 500 kaček a pak už jen rozjet pořádný pekla s dvojnásobným kreditem. \uD83D\uDE08\uD83D\uDC9A"));
         // Postačí, když si dneska 26. 6. dobiješ za 200 - 500 Kč mezi 17 a 19 a my ti aktivujem 2x tolik.🤩
         assertTrue(CheckServlet.textMatchesPattern("Postačí, když si dneska 26. 6. dobiješ za 200 - 500 Kč mezi 17 a 19 a my ti aktivujem 2x tolik.\uD83E\uDD29"));
+        // Stačí dnes 11. 7. naladit 200 - 500 kaček mezi 16 a 19 hodinou a Kaktus ti nabrnkne 2x takovej nářez.🔥
+        assertTrue(CheckServlet.textMatchesPattern("Stačí dnes 11. 7. naladit 200 - 500 kaček mezi 16 a 19 hodinou a Kaktus ti nabrnkne 2x takovej nářez.\uD83D\uDD25"));
     }
 }
